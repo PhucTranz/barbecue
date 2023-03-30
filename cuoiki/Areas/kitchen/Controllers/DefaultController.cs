2aff36e33f3610d64568a882f3d47a73e96bcc5a
Phuc <phuctran12421@gmail.com>
2023-03-30 14:44:33 +0700

    kitchen page

---
+++ cuoiki/Areas/kitchen/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cuoiki.Areas.kitchen.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: kitchen/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ cuoiki/Areas/kitchen/kitchenAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace cuoiki.Areas.kitchen
+{
+    public class kitchenAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "kitchen";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "kitchen_default",
+                "kitchen/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
