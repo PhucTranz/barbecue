5dc12bd2062e0b78ec8ce30cb0900f2c7637de2c
Phuc <phuctran12421@gmail.com>
2023-04-06 14:37:54 +0700

    design admin page

---
+++ cuoiki/Areas/admin/Data/js/table.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('#example').dataTable({
+        "language": {
+            "sProcessing": "Đang xử lý...",
+            "sLengthMenu": "Hiển thị _MENU_",
+            "sZeroRecords": "Không tìm thấy yêu cầu nào phù hợp",
+            "sInfo": "Hiển thị từ _START_ đến _END_ của _TOTAL_ đối tượng",
+            "sInfoEmpty": "",
+            "sInfoFiltered": "",
+            "sInfoPostFix": "",
+            "sSearch": "Tìm kiếm",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "Đầu",
+                "sPrevious": "Trước",
+                "sNext": "Tiếp",
+                "sLast": "Cuối"
+            }
+        },
+    });
+}); 
